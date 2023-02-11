dd651ae28ecce41f7b940814a58516bc1f77a522
KombinatorVit <slonik0775@gmail.com>
2023-02-11 20:15:35 +0200

    Initial commit

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "KombinatorVit",
+  "avatar": "https://avatars.githubusercontent.com/u/91082918?s=400&u=f7102d5429b709b80fe1b67becb6c626d16e35f7&v=4",
+  "name": "KombinatorVit",
+  "company": "сыч",
+  "blog": "porhub.com",
+  "location": "Dnipro",
+  "bio": "React/TS developer",
+  "twitter": "",
+  "repos": 55,
+  "followers": 4,
+  "following": 2,
+  "created": "2023-02-24T16:18:45Z",
+}
